9ce1a12eae869825a0335af46ef5e14c3554bcf0
mustafa.tikir <external.mustafa.tikir@kuehne-nagel.com>
2024-01-31 09:23:51 +0200

    Data loader improved & Product tests removed

--- product-service/src/main/java/com/taltech/ecommerce/productservice/util/DataLoader.java
+++ product-service/src/main/java/com/taltech/ecommerce/productservice/util/DataLoader.java
@@ -16,14 +16,28 @@ public class DataLoader implements CommandLineRunner {
     private final ProductRepository productRepository;
 
     @Override
-    public void run(String... args) throws Exception {
-        if (productRepository.count() < 1) {
+    public void run(String... args) {
+        if (productRepository.count() < 3) {
             Product product = new Product();
             product.setName("iPhone 13");
             product.setDescription("iPhone 13");
             product.setPrice(BigDecimal.valueOf(1000));
 
             productRepository.save(product);
+
+            Product product2 = new Product();
+            product2.setName("iPhone 15");
+            product2.setDescription("iPhone 15");
+            product2.setPrice(BigDecimal.valueOf(2000));
+
+            productRepository.save(product2);
+
+            Product product3 = new Product();
+            product3.setName("Samsung A50");
+            product3.setDescription("Samsung A50");
+            product3.setPrice(BigDecimal.valueOf(1500));
+
+            productRepository.save(product3);
         }
     }
 }

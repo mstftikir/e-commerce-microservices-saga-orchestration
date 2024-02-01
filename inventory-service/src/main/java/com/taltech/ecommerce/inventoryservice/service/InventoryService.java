4234d455c955c593462e0d43587a155b87d2db92
mustafa.tikir <external.mustafa.tikir@kuehne-nagel.com>
2024-02-01 10:37:27 +0200

    ResillienceJ deleted

--- inventory-service/src/main/java/com/taltech/ecommerce/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/taltech/ecommerce/inventoryservice/service/InventoryService.java
@@ -1,15 +1,15 @@
 package com.taltech.ecommerce.inventoryservice.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.taltech.ecommerce.inventoryservice.dto.InventoryResponse;
 import com.taltech.ecommerce.inventoryservice.repository.InventoryRepository;
 
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -19,7 +19,6 @@ public class InventoryService {
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
-    @SneakyThrows
     public List<InventoryResponse> isInStock(List<String> skuCode) {
         log.info("Checking Inventory");
         return inventoryRepository.findBySkuCodeIn(skuCode).stream()
--- order-service/src/main/java/com/taltech/ecommerce/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/taltech/ecommerce/orderservice/controller/OrderController.java
@@ -1,17 +1,17 @@
 package com.taltech.ecommerce.orderservice.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.taltech.ecommerce.orderservice.dto.OrderRequest;
 import com.taltech.ecommerce.orderservice.service.OrderService;
 
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
-import io.github.resilience4j.retry.annotation.Retry;
-import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/api/order")
@@ -23,16 +23,8 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
-    @TimeLimiter(name = "inventory")
-    @Retry(name = "inventory")
-    public CompletableFuture<String> placeOrder(@RequestBody OrderRequest orderRequest) {
+    public String placeOrder(@RequestBody OrderRequest orderRequest) {
         log.info("Placing Order");
-        return CompletableFuture.supplyAsync(() -> orderService.placeOrder(orderRequest));
-    }
-
-    public CompletableFuture<String> fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException) {
-        log.error("inventory fallbackMethod, orderRequest: {}, exception message: {}", orderRequest.toString(), runtimeException.getMessage());
-        return CompletableFuture.supplyAsync(runtimeException::getMessage);
+        return orderService.placeOrder(orderRequest);
     }
 }

80acf1f9b40e015b1a8beed29079223bccbb71e5
mustafa.tikir <external.mustafa.tikir@kuehne-nagel.com>
2024-02-02 14:51:22 +0200

    User and Payment services

--- discovery-server/src/main/java/com/taltech/ecommerce/discoveryserver/SecurityConfig.java
+++ discovery-server/src/main/java/com/taltech/ecommerce/discoveryserver/SecurityConfig.java
@@ -3,13 +3,18 @@ package com.taltech.ecommerce.discoveryserver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.csrf().ignoringRequestMatchers("/eureka/**");
+        httpSecurity
+            .csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/eureka/**").permitAll()
+                .anyRequest().authenticated());
         return httpSecurity.build();
     }
 }

e0786cba6e72bc9ce070180b7ee22332e9604d71
Mustafa Tikir <external.mustafa.tikir@kuehne-nagel.com>
2024-02-19 21:20:42 +0200

    Product deleted

--- product-service/src/main/java/com/taltech/ecommerce/productservice/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.taltech.ecommerce.productservice.repository;
-
-import com.taltech.ecommerce.productservice.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepository extends MongoRepository<Product, String> {
-}

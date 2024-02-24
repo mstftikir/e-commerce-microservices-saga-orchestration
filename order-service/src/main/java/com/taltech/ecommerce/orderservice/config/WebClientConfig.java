4bbfb1123f01636623c179505520d9bc99926d94
Mustafa Tikir <external.mustafa.tikir@kuehne-nagel.com>
2024-02-24 13:34:58 +0200

    Docker load balancing for webclient

--- order-service/src/main/java/com/taltech/ecommerce/orderservice/config/WebClientConfig.java
+++ order-service/src/main/java/com/taltech/ecommerce/orderservice/config/WebClientConfig.java
@@ -1,14 +1,23 @@
 package com.taltech.ecommerce.orderservice.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
 
     @Bean
-//    @LoadBalanced
+    @Profile("docker")
+    @LoadBalanced
+    public WebClient.Builder webClientBuilderLoadBalanced() {
+        return WebClient.builder();
+    }
+
+    @Bean
+    @Profile("!docker")
     public WebClient.Builder webClientBuilder() {
         return WebClient.builder();
     }

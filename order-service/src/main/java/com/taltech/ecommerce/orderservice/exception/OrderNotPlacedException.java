68f7a72dd606658d9906e6c61f5ea68d2396d3e9
Mustafa Tikir <external.mustafa.tikir@kuehne-nagel.com>
2024-03-02 19:52:02 +0200

    OrderNotPlacedException deleted

--- order-service/src/main/java/com/taltech/ecommerce/orderservice/exception/OrderNotPlacedException.java
+++
@@ -1,8 +0,0 @@
-package com.taltech.ecommerce.orderservice.exception;
-
-public class OrderNotPlacedException extends RuntimeException {
-
-    public OrderNotPlacedException(String message) {
-        super(message);
-    }
-}
